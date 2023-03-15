0021c43667eefdc1b09012a0248a1e735165fbc8
Bruno Santos Silva <jkbrunowk@hotmail.com>
2023-03-14 21:35:10 -0300

    Adicionando Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import * as C from './AppStyles';
-
-
+import './index.css';
 const App = () => {
 
   return (
   <>
-  <div className='box_div'>
-    <h1>Bem vindo ao nosso time, confirme para continuar!</h1>
-    <p>Experimente a diferença com AuraFinance - o banco digital que oferece soluções financeiras personalizadas e fáceis de usar.
-       Junte-se a nós hoje e comece a gerenciar suas finanças de forma mais inteligente e prática.</p>
-       <C.Container>Texto do componente </C.Container>
-  </div>
-      
-  
+      <h1 className=''>Oi, tudo bem?</h1>
   </>    
   )
 }
--- src/componets/Botao/index.tsx
+++
@@ -1,8 +0,0 @@
-import './style.css';
-export const Botao = () => {
-    return (
-        <>
-            <button className='botao' >Confirmar!</button>
-        </>
-    )
-}
