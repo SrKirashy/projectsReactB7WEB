562ac2e0fb70775423126fff10c1b93a86fbbead
Bruno Santos Silva <jkbrunowk@hotmail.com>
2023-03-23 22:18:44 -0300

    Criando um sistema de mostrar filmes em cartaz, atráves de requisição com metodo GET.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import './index.css';
+import { useState } from "react";
+import { Movie } from "./types/movies";
+import styles from "./style.module.css";
 const App = () => {
+  const [movie, setMovie] = useState<Movie[]>([]);
+
+  const handlerMovies = () => {
+    fetch('https://api.b7web.com.br/cinema/')
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setMovie(json);
+      })
+  }
+
 
   return (
-  <>
-      <h1 className=''>Oi, tudo bem?</h1>
-  </>    
+    <>
+      <div className={styles.container}>
+        <button className={styles.button} onClick={handlerMovies}>Mostrar filmes.</button>
+        <p>Quantidade de filmes em cartaz: {movie.length}</p>
+        <div className={styles.containerTableMovies}>
+          {movie.map((item, index) => (
+            <div className={styles.containerMovies}>
+              <img src={item.avatar} className={styles.img} />
+              <p>{item.titulo}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 
