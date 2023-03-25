ff191dd45aa6202695cd63c58bef360618eae523
Bruno Santos Silva <jkbrunowk@hotmail.com>
2023-03-24 22:32:05 -0300

    Criando tela de carregamento, estilisando aba de filmes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Movie } from "./types/movies";
 import styles from "./style.module.css";
 const App = () => {
-  const [movie, setMovie] = useState<Movie[]>([]);
 
-  const handlerMovies = () => {
-    fetch('https://api.b7web.com.br/cinema/')
-      .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        setMovie(json);
-      })
+  useEffect(() => {
+    handlerMovies();
+  }, [])
+
+  const [movie, setMovie] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handlerMovies = async () => {
+    setLoading(true);
+    let res = await fetch('https://api.b7web.com.br/cinema/');
+    let json = await res.json();
+    setLoading(false);
+    setMovie(json);
   }
 
 
   return (
     <>
-      <div className={styles.container}>
-        <button className={styles.button} onClick={handlerMovies}>Mostrar filmes.</button>
-        <p>Quantidade de filmes em cartaz: {movie.length}</p>
-        <div className={styles.containerTableMovies}>
-          {movie.map((item, index) => (
-            <div className={styles.containerMovies}>
-              <img src={item.avatar} className={styles.img} />
-              <p>{item.titulo}</p>
-            </div>
-          ))}
+      {loading &&
+        <>
+          <div className={styles.container}>
+            <div className={styles.circle}></div>
+          </div>
+        </>
+      }
+
+      {!loading &&
+        <div className={styles.container}>
+          <p>Quantidade de filmes em cartaz: {movie.length}</p>
+          <div className={styles.containerTableMovies}>
+            {movie.map((item, index) => (
+              <div className={styles.containerBlock}>
+                <div className={styles.containerMovies}>
+                  <div className={styles.description}>
+                    <h1>{item.titulo}</h1>
+                    <p className={styles.p} >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  </div>
+                  <img src={item.avatar} className={styles.img} />
+                  <p>{item.titulo}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      }
     </>
   )
 }
